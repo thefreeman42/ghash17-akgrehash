113bf761447bc03c0dc9ac6138ea0290038abf43
Titusz Ban <Titusz Ban>
2017-02-23 19:27:26 +0000

    generator: random generation creation

--- generator.py
+++ generator.py
@@ -1,30 +1,58 @@
-import numpy
+import numpy as np
 from numpy.random import choice
 
 class Generator:
     videos = []
     video_count = 0
     cache_count = 0
+    cache_size = 0
 
-    def __init__(self, videos, cache_count):
+    prob_dist = []                                  # video selection probability distribution
+
+    size_consideration_ratio = 0.2
+
+    def __init__(self, videos, cache_count, cache_size):
         self.videos = videos
         self.video_count = len(videos)
         self.cache_count = cache_count
+        self.cache_size = cache_size
+        sum_r = float(sum(requests for size, requests in videos))
+        sum_s = float(sum(size for size, requests in videos))
+        for size, requests in videos:
+            self.prob_dist.append(float(size) / sum_s * self.size_consideration_ratio + float(requests) / sum_r  * (1.0 - self.size_consideration_ratio) )
 
-    def New_Speciment(self):
+    def new_specimen(self):                        # random specimen
+        spec = np.zeros(shape = (self.cache_count, self.video_count), dtype=np.int)
+        for i in range(self.cache_count):
+            available_size = self.cache_size
+            valid = True
+            vids = []
+            while valid:
+                vid = choice(range(self.video_count), 1, self.prob_dist)
+                if vid in vids:
+                    valid = False
+                elif self.videos[vid][0] > available_size:
+                    valid = False
+                else:
+                    available_size -= self.videos[vid][0]
+                    vids.append(vid)
+                    spec[i, vid] = 1
+        return spec
+
+    def new_specimen(self, parentA, parentB):      # genetic specimen
         pass
 
+    def random_generation(self, speciment_count):   # randomised generation of specimens
+        generation = []
+        for i in range(speciment_count):
+            generation.append(self.new_specimen())
+        return generation
 
-    def New_Speciment(self, parentA, parentB):
+    def new_generation(self, speciment_count, previous_best):   # genetic generation of specimens
         pass
 
-    def Random_Generation(self, speciment_count):
-        pass
-
-    def New_Generation(self, speciment_count, previous_best):
-        pass
-    def Get_Generation(self, speciment_count, previous_best):
+    def get_generation(self, speciment_count, previous_best):   # get new generation
         if previous_best == []:
             return self.Random_Generation(speciment_count)
-        else
+        else:
             return self.New_Generation(speciment_count, previous_best)
